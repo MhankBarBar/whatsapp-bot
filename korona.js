d388e52998caccad69ec39ca59abe7e68cb35e82
ムハマド・ロイヤニ <55822959+MhankBarBar@users.noreply.github.com>
2020-08-12 10:46:44 +0700

    .

--- korona.js
+++ korona.js
@@ -2,13 +2,13 @@ const axios = require('axios').default;
 
 module.exports = async () => {
 	try {
-		const response = await axios.get('https://api.kawalcorona.com/indonesia/');
+		const response = await axios.get('https://ostch.herokuapp.com/api/v1/corona/');
 		const { positif, sembuh, meninggal, dirawat } = response.data[0];
-		let korona = 'Data Korona Indonesia Saat ini ??\n\n';
-		korona += `?? Positif : ${positif}\n`;
-		korona += `?? Sembuh : ${sembuh}\n`;
-		korona += `?? Meninggal : ${meninggal}\n`;
-		korona += `?? Dirawat : ${dirawat} \n`;
+		let korona = 'Data Korona Indonesia Saat ini 🦠\n\n';
+		korona += `😔 Positif : ${positif}\n`;
+		korona += `😊 Sembuh : ${sembuh}\n`;
+		korona += `😭 Meninggal : ${meninggal}\n`;
+		korona += `🤒 Dirawat : ${dirawat} \n`;
 		// console.log(korona);
 		return korona;
 	} catch (error) {
