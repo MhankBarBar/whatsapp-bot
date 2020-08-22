b6fa06d6d6189439bc52e39ffb9941fc6b1a688d
MhankBarBar <royani7896@gmail.com>
2020-08-22 20:05:00 +0000

    update

--- index.js
+++ index.js
@@ -72,15 +72,15 @@ async function msgHandler (client, message) {
         const { type, body, from, t, sender, isGroupMsg, chat, groupMetadata, caption, isMedia, mimetype, quotedMsg } = message
         const { id, pushname } = sender
         const { name } = chat
-	const names = sender.pushname || chat.name || sender.verifiedName || '';	
+	const pushnames = sender.pushname || chat.name || sender.verifiedName || '';	
         const time = moment(t * 1000).format('DD/MM HH:mm:ss')
-        const commands = ['!sticker', '!stiker', '!hello','!info','!covid','!join','!lirik','!quotemaker','!help','!owner','!meme','!add','!kick','!leave','!promote','!demote','!admin','!linkGrup','!revLinkGrup','!Seasonal anime','!neko','!wallpaper','Heave ho','Heave ho!','!quote','!quotes','!Quote','!weather','#Tod','!Pokemon','!waifu','!Waifu','!fb','!ytmp3','!ig']
+        const commands = ['!sticker', '!stiker', '!hello','!info','!covid','!join','!lirik','!quotemaker','!help','!owner','!meme','!setPic','!add','!kick','!leave','!promote','!demote','!admin','!linkGrup','!revLinkGrup','!Seasonal anime','!neko','!wallpaper','Heave ho','Heave ho!','!quote','!quotes','!Quote','!weather','#Tod','!Pokemon','!waifu','!Waifu','!fb','!ytmp3','!ig','!gifSticker','!gifStiker']
         const cmds = commands.map(x => x + '\\b').join('|')
         const cmd = type === 'chat' ? body.match(new RegExp(cmds, 'gi')) : type === 'image' && caption ? caption.match(new RegExp(cmds, 'gi')) : ''
 
         if (cmd) {
-            if (!isGroupMsg) console.log(color('[EXEC]'), color(time, 'yellow'), color(cmd[0]), 'from', color(names))
-            if (isGroupMsg) console.log(color('[EXEC]'), color(time, 'yellow'), color(cmd[0]), 'from', color(names), 'in', color(name))
+            if (!isGroupMsg) console.log(color('[EXEC]'), color(time, 'yellow'), color(cmd[0]), 'from', color(pushname))
+            if (isGroupMsg) console.log(color('[EXEC]'), color(time, 'yellow'), color(cmd[0]), 'from', color(pushname), 'in', color(name))
             const args = body.trim().split(' ')
             switch (cmd[0]) {
                 case '!sticker':
@@ -103,14 +103,63 @@ async function msgHandler (client, message) {
                             client.sendText(from, 'Url is invalid')
                         }
                     } else {
-                        client.reply(from, `You did not quote a picture, *${names}* Baka! To make a sticker, send an image with "!sticker" as caption`, message)
+                        client.reply(from, `Dasar ${pushname} bodoh!, yang mau dijadiin stiker apa? lu cuma send teks *!stiker*. kalo mau bikin stiker send foto dengan caption *!stiker*.`, message)
                     }
-                    break
+		    break
+		/*case '!gifSticker':
+		case '!gifStiker':
+			    if (args.length == 1){
+                		const url = args[0]
+                		const isMediaGiphy = url.match(new RegExp(/https?:\/\/media.giphy.com\/media/, 'gi'));
+                		const isGiphy = url.match(new RegExp(/https?:\/\/(www\.)?giphy.com/, 'gi'));
+                		if(isGiphy){
+                    			const getGiphyCode = url.match(new RegExp(/\-(?:.(?!\-))+$/, 'gi'));
+                    			if(getGiphyCode){
+                        			let delHyphen = getGiphyCode[0].replace(/-/gi, "");
+                        			const smallGif = "https://media.giphy.com/media/"+delHyphen+"/giphy-downsized.gif";
+                        			await client.sendGiphyAsSticker(from, smallGif)
+                        			.catch((err) => {
+                            			console.log(err)
+                        			})
+                    			} else {
+                        			client.reply(from, "Gagal membuat sticker gif", id)
+                    			}
+                		} else if(isMediaGiphy){
+                    			const normalGif = url.match(new RegExp(/(giphy|source).(gif|mp4)/, 'gi'));
+                    			if(normalGif){
+                        			let smallGif = url.replace(normalGif[0], "giphy-downsized.gif")
+                        			await client.sendGiphyAsSticker(from, smallGif)
+                        			.catch((err) => {
+                            			console.log(err)
+                        			})
+                    			}
+                		} else {
+                    			client.reply(from, "Saat ini sticker gif hanya bisa menggunakan link giphy saja kak.", id)
+                		}
+            		}
+                    break*/
+		case '!setPic':
+			    if(isGroupMsg) {
+				    if(isMedia) {
+					    var wkk = `${from.split('-')[0]}@c.us`
+					    if(message.author == wkk || message.author == '6285892766102@c.us') {
+					    	const mediaData = await decryptMedia(message)
+					    	const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
+					    	await client.setGroupIcon(from, imageBase64)
+					    }else{
+						 client.reply(from, 'Fitur ini hanya bisa digunakan oleh admin[owner]', message)
+					    }
+				    }
+			    } else {
+				    client.reply(from, 'Fitur ini hanya bisa di gunakan dalam grup', message)
+			    }
+		    break
                 case '!hello':
-                        client.reply(from, `Hello *${names}*, How can I help?`, message)
+                        client.reply(from, `Hello *${pushname}*, How can I help?`, message)
                     break
 		case '#tod':
 			client.sendImageAsStickerGif(from, 'https://i.imgur.com/31zUM5g.gif')
+			client.sendRawWebpAsSticker(from, 'https://i.giphy.com/media/LMt9638dO8dftAjtco/200.webp')
 			client.sendStickerfromUrl(from, 'https://media.tenor.com/images/62c4b269d97c2412c4f364945f62afae/tenor.gif', {method: 'get'})			
                     break
 		/*case '!covid':
@@ -121,7 +170,9 @@ async function msgHandler (client, message) {
 			if (args.length >=2){
                         	const url = args[1]
                         	const result = await ytmp3(url)
+				console.log(result)
                         	client.sendFileFromUrl(from, result , 'audio.mp3')
+				//client.sendPtt(from, result, 'audio.mp3')
 			}else{
 				client.reply(from, 'usage:\n\n!ytmp3 https://youtu.be/6l5V3BWDcMw', message)
 }
@@ -153,12 +204,13 @@ async function msgHandler (client, message) {
                         	client.sendText(from, result)
 }
                     break
-		case '!quotemaker':            
-                        if (args.length == 4) {
+		case '!quotemaker':
+			arg = body.trim().split('|')
+                        if (arg.length == 4) {
 				client.sendText(from, 'Tunggu yaa, sedang proses . . .') 
-                        	const quotes = args[1]
-                        	const author = args[2]
-                        	const theme = args[3]
+                        	const quotes = arg[1]
+                        	const author = arg[2]
+                        	const theme = arg[3]
                         	const result = await quotemaker(quotes, author, theme)
                         	client.sendFile(from, result, 'quotesmaker.jpg','neh...')
 }
@@ -177,27 +229,31 @@ async function msgHandler (client, message) {
 			if(isGroupMsg){
 				if(args.length >=2){
 					var wkwk = `${from.split('-')[0]}@c.us`
-					if(message.author === wkwk ){
+					if(message.author == wkwk || message.author == '6285892766102@c.us'){
 						org = args[1]
 						client.addParticipant(from,`${org}@c.us`)
 					}else{
 						client.reply(from, 'Fitur ini hanya bisa di pakai oleh admin[owner]', message)
-		}
-	}
-}
+					}
+				}
+			}else{
+				client.reply(from, 'Fitur ini hanya bisa di gunakan dalam grup', message)
+			}
 		    break
 		case '!kick':
 			if(isGroupMsg){
 				if(args.length >=2){
 					var wkwk = `${from.split('-')[0]}@c.us`
-					if(message.author === wkwk ){
-						wong = args[1]
+					if(message.author == wkwk || message.author == '6285892766102@c.us' ){
+						wong = body.split('@')[1] || args[1]
 						client.removeParticipant(from,`${wong}@c.us`)
 					}else{
 						client.reply(from, 'Fitur ini hanya bisa di pakai oleh admin[owner]', message)
-		}
-	}
-}
+					}
+				}
+			}else{
+				client.reply(from, 'Fitur ini hanya bisa di gunakan dalam grup', message)
+			}
 		    break
 	/*	case '!leave':
 			if(isGroupMsg){
@@ -209,40 +265,57 @@ async function msgHandler (client, message) {
 			if(isGroupMsg){
 				if(args.length >=2){
 					var wkwk = `${from.split('-')[0]}@c.us`
-					if(message.author === wkwk ){
-						prom = args[1]
+					if(message.author == wkwk || message.author == '6285892766102@c.us' ){
+						prom = body.split('@')[1] || args[1]
 						client.promoteParticipant(from,`${prom}@c.us`)
 					}else{
 						client.reply(from, 'Fitur ini hanya bisa di pakai oleh admin[owner]', message)
-		}
-	}
-}
+					}
+				}
+			}else{
+				client.reply(from, 'Fitur ini hanya bisa di gunakan dalam grup', message)
+			}
 		    break
 		case '!demote':
 			if(isGroupMsg){
 				if(args.length >=2){
 					var wkwk = `${from.split('-')[0]}@c.us`
-					if(message.author === wkwk ){
-						demo = args[1]
+					if(message.author == wkwk || message.author == '6285892766102@c.us' ){
+						demo = body.split('@')[1] || args[1]
 						client.demoteParticipant(from,`${demo}@c.us`)
 					}else{
 						client.reply(from, 'Fitur ini hanya bisa di pakai oleh admin[owner]', message)
-		}
-	}
-}
+					}
+				}
+			}else{
+				client.reply(from, 'Fitur ini hanya bisa di gunakan dalam grup', message)
+			}
 		    break
 		case '!revLinkGrup':
 			if(isGroupMsg){
-				client.revokeGroupInviteLink(chat.id);
-				client.sendText(from, 'Tautan undangan berhasil di tarik')
+				try {
+					await client.revokeGroupInviteLink(chat.id);
+					//client.sendText(from, 'Tautan undangan berhasil di tarik')
+				} catch (e) {
+					client.reply(from, 'Sepertinya bot belum menjadi admin', message)
+				}
 }
 		    break
 		case '!join':
 			if (args.length >=2) {
 				const link = args[1]
-				client.inviteInfo(link);
-				client.joinGroupViaLink(link)
-				client.sendText(from, 'Otw join gan')
+				const invite = link.replace('https://chat.whatsapp.com/', '')
+				if (link.match(/(https:\/\/chat.whatsapp.com)/gi)) {
+					try {
+						await client.inviteInfo(invite);
+						await client.joinGroupViaLink(invite)
+						client.reply(from, 'Otw join gan', message)
+					} catch (e) {
+						client.reply(from, 'Sepertinya link grup bermasalah', message)
+					}
+				} else {
+					client.reply(from, 'Ini link????', message)
+				}
 }
 		    break
                     
@@ -295,7 +368,7 @@ async function msgHandler (client, message) {
                     }
                     break
 		case '!help':
-                        client.sendText(from, `Hi *${names}*\n\nPerintah\n\n!sticker \nMengubah gambar ke stiker\n\n!neko\nMengirim gambar kucing acak\n\n!Pokemon Mengirim gambar pokemon acak \n \n!wallpaper \nMengirim wallpaper anime acak (beta)\n\n!Seasonal anime \nMenampilkan daftar anime musiman\n\n!info \nMenampilkan syarat dan ketentuan\n\n!quote\nMengirim quotedd\n\n!waifu\nMengirim gambar anime \n\n!linkGrup\nMengambil tautan undangan grup, [ bot admin ]\n\n!revLinkGrup\nMencabut tautan undangan saat ini, [ bot admin ]\n\n!join https://chat.whatsapp.com/blablabla\nUntuk menambahkan bot ke grup anda\n\n!lirik aku-bukan-boneka\nMenampilkan lirik lagu aku bukan boneka\n\n!ytmp3 https://youtu.be/6l5V3BWDcMw\nMendownload mp3 dari YouTube\n\n[ Owner Only ]\n\n!add 628xxxx\nUntuk menambahkan anggota grup\n\n!kick 628xxx\nUntuk mengeluarkan member grup\n\n!promote 628xxx\nMenaikkan pangkat member menjadi admin\n\n!demote 628xxx\nMenurunkan pangkat admin menjadi member`)
+                        client.sendText(from, `Hi *${pushname}*\n\nPerintah\n\n!sticker \nMengubah gambar ke stiker\n\n!neko\nMengirim gambar kucing acak\n\n!Pokemon\nMengirim gambar pokemon acak \n \n!wallpaper \nMengirim wallpaper anime acak (beta)\n\n!Seasonal anime \nMenampilkan daftar anime musiman\n\n!info \nMenampilkan syarat dan ketentuan\n\n!quote\nMengirim quotedd\n\n!waifu\nMengirim gambar anime \n\n!linkGrup\nMengambil tautan undangan grup, [ bot admin ]\n\n!revLinkGrup\nMencabut tautan undangan saat ini, [ bot admin ]\n\n!join https://chat.whatsapp.com/blablabla\nUntuk menambahkan bot ke grup anda\n\n!lirik aku-bukan-boneka\nMenampilkan lirik lagu aku bukan boneka\n\n!ytmp3 https://youtu.be/6l5V3BWDcMw\nMendownload mp3 dari YouTube\n\n[ Owner Only ]\n\n!add 628xxxx\nUntuk menambahkan member grup\n\n!kick @tag\nUntuk mengeluarkan member grup\n\n!promote @tag\nMenaikkan pangkat member menjadi admin\n\n!demote @tag\nMenurunkan pangkat admin menjadi member\n\n!setPic\nMengubah icon grup, kirim foto dengan caption !setPic`)
                     break
 		case '!Seasonal anime':
                         client.sendText(from, 'Summer 2020 \n Re:Zero kara Hajimeru Isekai Seikatsu 2nd Season \n Yahari Ore no Seishun Love Comedy wa Machigatteiru. Kan \n The God of High School \n Sword Art Online: Alicization - War of Underworld 2nd Season \n Enen no Shouboutai: Ni no Shou \n Maou Gakuin no Futekigousha: Shijou Saikyou no Maou no Shiso, Tensei shite Shison-tachi no Gakkou e \n Kanojo, Okarishimasu \n Deca-Dence \n Uzaki-chan wa Asobitai! \n Monster Musume no Oishasan')
@@ -307,8 +380,8 @@ async function msgHandler (client, message) {
                         client.sendText(from, 'Ini adalah program sumber terbuka yang ditulis dalam Javascript. \n \nDengan menggunakan bot, Anda menyetujui Syarat dan Ketentuan kami \n \nSyarat dan ketentuan \n \nTeks dan nama pengguna whatsapp Anda akan disimpan di server kami selama bot aktif, data Anda akan dihapus ketika  bot menjadi offline.  Kami TIDAK menyimpan gambar, video, file audio dan dokumen yang Anda kirim.  Kami tidak akan pernah meminta Anda untuk mendaftar atau meminta kata sandi, OTP, atau PIN Anda.  \n \n Terima kasih, Selamat bersenang-senang!')    
                      }
         } else {
-            if (!isGroupMsg) console.log('[RECV]', color(time, 'yellow'), 'Message from', color(pushname))
-            if (isGroupMsg) console.log('[RECV]', color(time, 'yellow'), 'Message from', color(pushname), 'in', color(name))
+            if (!isGroupMsg) console.log('[RECV]', color(time, 'yellow'), color(body, 'red'), color(pushname))
+            if (isGroupMsg) console.log('[RECV]', color(time, 'yellow'), color(body, 'red'), color(pushname), 'in', color(name))
         }
     } catch (err) {
         console.log(color('[ERROR]', 'red'), err)
