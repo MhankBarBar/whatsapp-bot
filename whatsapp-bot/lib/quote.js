19d3cce6efa7442f3289cd5141ab987bdfe93ff5
MhankBarBar <royani7896@gmail.com>
2020-09-25 10:58:56 +0700

    Update + fixed bug

--- whatsapp-bot/lib/quote.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-module.exports = () => {
-  const data = fs.readFileSync('./lib/quote.json');
-  const jsonData = JSON.parse(data);
-  const randIndex = Math.floor(Math.random() * jsonData.length);
-  const randKey = jsonData[randIndex];
-  return `${randKey.quote} - ${randKey.by}`;
-};
