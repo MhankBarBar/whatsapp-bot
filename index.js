6a0628e9c82db543710e399caa4ef5322f15d0fe
MhankBarBar <royani7896@gmail.com>
2020-10-03 21:41:42 +0700

    update ✓ add new features ✓ fixed ✓

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const { create, Client } = require('@open-wa/wa-automate')
+const welcome = require('./lib/welcome')
 const msgHandler = require('./msgHndlr')
 const options = require('./options')
 
-const start = (client = new Client()) => {
+const start = async (client = new Client()) => {
         console.log('[SERVER] Server Started!')
         // Force it to keep the current session
         client.onStateChanged((state) => {
-                console.log('[Client State]', state)
-                if (state === 'CONFLICT') client.forceRefocus()
-                if (state === 'UNLAUNCHED') client.forceRefocus()
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
         })
         // listening on message
-        client.onMessage( async (message) => {
+        client.onMessage((async (message) => {
             client.getAmountOfLoadedMessages()
-                .then((msg) => {
-                    if (msg >= 3000) {
-                        console.log('[CLIENT]', `Loaded Message Reach ${msg}, cuting message cache...`)
-                        client.cutMsgCache()
-                    }
-                })
-            await msgHandler(client, message)
-        })
+            .then((msg) => {
+                if (msg >= 3000) {
+                    client.cutMsgCache()
+                }
+            })
+            msgHandler(client, message)
+        }))
 
+        client.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(client, heuh)
+            //left(client, heuh)
+            }))
         
-        client.onAddedToGroup((async (chat) => {
-            let totalMem = await chat.groupMetadata.participants.length
+        client.onAddedToGroup(((chat) => {
+            let totalMem = chat.groupMetadata.participants.length
             if (totalMem < 30) { 
             	client.sendText(chat.id, `Cih member nya cuma ${totalMem}, Kalo mau invite bot, minimal jumlah mem ada 30`).then(() => client.leaveGroup(chat.id)).then(() => client.deleteChat(chat.id))
             } else {
@@ -32,10 +35,10 @@ const start = (client = new Client()) => {
             }
         }))
 
-        client.onAck((x => {
+        /*client.onAck((x => {
             const { from, to, ack } = x
             if (x !== 3) client.sendSeen(to)
-        }))
+        }))*/
 
         // listening on Incoming Call
         client.onIncomingCall(( async (call) => {
@@ -45,5 +48,6 @@ const start = (client = new Client()) => {
     }
 
 create('BarBar', options(true, start))
+//create('BarBar')
     .then(async client => await start(client))
     .catch((error) => console.log(error))
