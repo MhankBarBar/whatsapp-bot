aff615db2239b9373c72ad231c2382059a34e942
f4yyy <74439661+f4yyy@users.noreply.github.com>
2020-11-14 09:46:35 +0700

    Ok

--- msgHndlr.js
+++ msgHndlr.js
@@ -65,7 +65,7 @@ module.exports = msgHandler = async (client, message) => {
         const groupAdmins = isGroupMsg ? await client.getGroupAdmins(groupId) : ''
         const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
         const isBotGroupAdmins = isGroupMsg ? groupAdmins.includes(botNumber + '@c.us') : false
-        const ownerNumber = ["089648693646"] // replace with your whatsapp number
+        const ownerNumber = ["6289648693646"]
         const isOwner = ownerNumber.includes(sender.id)
         const isBlocked = blockNumber.includes(sender.id)
         const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
