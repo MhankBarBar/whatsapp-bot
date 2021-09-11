35783c2b87ab62390839458e3b4fc263f3bf2244
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2021-09-11 13:59:19 -0300

    accelerates  speed creating animated sticker
    accelerates by 95% the speed of creating an animated sticker, eliminating the need to save the file before generating the sticker

--- msgHndlr.js
+++ msgHndlr.js
@@ -102,20 +102,23 @@ module.exports = msgHandler = async (client, message) => {
         case '!stickergif':
         case '!stikergif':
         case '!sgif':
-            if (isMedia) {
-                if (mimetype === 'video/mp4' && message.duration < 10 || mimetype === 'image/gif' && message.duration < 10) {
-                    const mediaData = await decryptMedia(message, uaOverride)
-                    client.reply(from, '[WAIT] Sedang di proses⏳ silahkan tunggu ± 1 min!', id)
-                    const filename = `./media/aswu.${mimetype.split('/')[1]}`
-                    await fs.writeFileSync(filename, mediaData)
-                    await exec(`gify ${filename} ./media/output.gif --fps=30 --scale=240:240`, async function (error, stdout, stderr) {
-                        const gif = await fs.readFileSync('./media/output.gif', { encoding: "base64" })
-                        await client.sendImageAsSticker(from, `data:image/gif;base64,${gif.toString('base64')}`)
-                    })
-                } else (
+			
+	if (isMedia) {
+
+	    if (mimetype === 'video/mp4' && message.duration < 30 || mimetype === 'image/gif' && message.duration < 30) {
+		const mediaData = await decryptMedia(message, uaOverride)
+	        client.reply(from, '[WAIT] Sedang di proses⏳ silahkan tunggu ± 1 sec!', id)
+
+		const filename = `./media/aswu.${mimetype.split('/')[1]}`
+			await fs.writeFileSync(filename, mediaData)
+			await exec(`gify ${filename} ./media/output.gif --fps=30 --scale=240:240`, async function (error, stdout, stderr) {
+			const gif = await fs.readFileSync('./media/output.gif', { encoding: "base64" })
+			await client.sendImageAsSticker(from, `data:image/gif;base64,${gif.toString('base64')}`)
+		})
+	    } else (
                     client.reply(from, '[❗] Kirim video dengan caption *!stickerGif* max 10 sec!', id)
-                )
-            }
+	    )
+	}
             break
 	    case '!stickernobg':
         case '!stikernobg':
